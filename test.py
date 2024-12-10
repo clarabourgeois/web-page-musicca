ffe662195a00fca9f5a8953812cb5cd62df2414d
Clara BOURGEOIS Dev-Mind <clara.bourgeois@etu.emse.fr>
2024-12-10 12:19:01 +0100

    Test commit to trigger lint errors

---
+++ test.py
@@ -0,0 +1,16 @@
+def bad_function():
+    x =  10
+    y=20
+    if x==y:
+        print("X is equal to Y")
+    else:
+        print("X is not equal to Y")
+  # Indentation error here
+
+class  MyClass:  # Extra space before the class name
+ def __init__(self):
+  self.name = "test"  # Indentation error
+
+ def bad_method(self):
+     print("Hello World")
+     return 42
